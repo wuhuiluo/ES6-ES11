7134868db1e6de7e1e38881b91292f9334afc6e3
wuhuiluo <510426343@qq.com>
2020-07-14 23:49:32 +0800

    latest commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const arr = [1,2,3]
+// for(let i = 0 ; i < arr.length ; i++) {
+//     console.log(arr[i])
+// }
+// let result = arr.map(value => {
+//     value += 1 
+//     return value
+// })
+// console.log(result)
+arr.reduce(())
+
