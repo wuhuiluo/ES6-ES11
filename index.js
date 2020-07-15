0abf5e994dd3bec43a797c1ce27c3113db141175
wuhuiluo <510426343@qq.com>
2020-07-15 20:07:05 +0800

    latest commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const arr = [1,2,3]
+const arr = [1, 2,3, 4]
 // for(let i = 0 ; i < arr.length ; i++) {
 //     console.log(arr[i])
 // }
@@ -7,5 +7,14 @@ const arr = [1,2,3]
 //     return value
 // })
 // console.log(result)
-arr.reduce(())
-
+// let result = arr.map(value => {
+//     return value
+// }) 
+// console.log(result);
+// let result = arr.reduce((prev,cur) => {
+//     prev.indexOf(cur) == -1 && prev.push(cur)
+//     return prev
+// }, [])
+for(item of arr) {
+    console.log(item);
+}
